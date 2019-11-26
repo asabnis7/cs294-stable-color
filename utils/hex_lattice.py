4691aaf699b67ed1c5b9368844fd9dc09fe4587b
asabnis7 <arjunsabnis@gmail.com>
2019-11-26 01:06:01 -0800

    Changed blue mosaic to be more regular

--- utils/hex_lattice.py
+++ utils/hex_lattice.py
@@ -48,22 +48,25 @@ def gen_color_lattice(r, g, b, w, a=1.):
     # Random center
     i0, j0 = (np.random.rand(2) - 0.5)
     XE, YE = XE + i0 * a, YE + i0 * a
-    
+
     n = len(XE)
-    ratio = [int(n*r), int(n*g), int(n*b)]
+    ratio = [int(n*r), int(n*g)]
     r = random.sample(range(0, n), n)
-    cones = [r[0:ratio[0]], r[ratio[0]:ratio[0]+ratio[1]], r[ratio[0]+ratio[1]:n]]
-   
+    rb = (np.asarray(range(0, n, int(n/(b*n)))) + random.randint(0,20)) % n
+    for i in range(0, len(rb)):
+        r.remove(rb[i])
+
+    cones = [r[0:ratio[0]], r[ratio[0]:len(r)], rb] 
     XE = [XE[cones[0]], XE[cones[1]], XE[cones[2]]]
     YE = [YE[cones[0]], YE[cones[1]], YE[cones[2]]]
-    
+
     return XE, YE
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     #for i in range(10):i
     #XE, YE = gen_hex_lattice(8.00, 0.5)
-    XE, YE = gen_color_lattice(0.47, 0.47, 0.6, 7.00, 0.5)
+    XE, YE = gen_color_lattice(0.47, 0.47, 0.06, 7.00, 0.5)
     
     # Trichromatic
     #fig, ax = plt.subplots(2,2)
@@ -90,10 +93,10 @@ if __name__ == '__main__':
 
     #plt.show()
 
-    #plt.scatter(XE[0], YE[0], color='red')
-    #plt.scatter(XE[1], YE[1], color='green')
-    #plt.scatter(XE[2], YE[2], color='blue')
-    #plt.axes().set_aspect('equal')
-    #plt.show()
+    plt.scatter(XE[0], YE[0], color='red')
+    plt.scatter(XE[1], YE[1], color='green')
+    plt.scatter(XE[2], YE[2], color='blue')
+    plt.axes().set_aspect('equal')
+    plt.show()
 
 #    idx = (-w < XE) * (XE < w) * (-w < YE) * (YE < w)
